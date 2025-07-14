59e127aa9776f7e25c21af8904044226661c5ac0
hoangvu1118 <mhoang.vuduc@gmail.com>
2025-07-14 09:54:27 +0700

    Done javascript datatype and syntax

---
+++ Javascript/Variable/home.js
@@ -0,0 +1,33 @@
+console.log("Hello, The first day | day one -> one day")
+
+let name1 = "Hoang"
+let surname = "Vu"
+
+console.log(name1)
+console.log(surname)
+
+const pi = 3.14;
+console.log(pi)
+
+name1 = (3 + 2) - 76 * (1 + 1)
+console.log("Result")
+console.log(name1)
+
+// Assignment Variable
+console.log("Assignment!")
+console.log(23+97 + 18+ 11 +20+ 29 + 30)
+console.log((4 + 6 + 9) / 77)
+
+console.log(typeof name1)
+console.log(typeof surname)
+console.log(typeof pi)
+
+
+const lotsOfDecimal = 1.7665849587;
+console.log(lotsOfDecimal)
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+console.log(twoDecimalPlaces)
+
+let a1 = '5'
+let b1 = 7 - 2
+console.log(a1 !== b1)
