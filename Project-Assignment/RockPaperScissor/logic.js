ce326116f14f3e2d1c7fbfcea75f2f0a172e3454
hoangvu1118 <mhoang.vuduc@gmail.com>
2025-07-20 23:49:18 +0700

    Adding Logic for RPS game
    Finish the feature - playing a round with 5 games

--- Project-Assignment/RockPaperScissor/logic.js
+++ Project-Assignment/RockPaperScissor/logic.js
@@ -1,5 +1,9 @@
 let randomNumber;
-let userChoice;
+let computerChocie
+
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice(){
     randomNumber = Math.floor(Math.random() * 3) + 1;
     if(randomNumber == 1){
@@ -12,4 +16,50 @@ function getComputerChoice(){
         return "Scissor"
     }
 }
-console.log(getComputerChoice())
+
+function playRound(){
+    computerChocie = getComputerChoice()
+    computer = computerChocie.toLocaleLowerCase()
+
+    let userChoice = prompt("What is ur choice (Rock | Paper | Scissor) ?")
+    human = userChoice.toLocaleLowerCase()
+
+    if(computer == human){
+        console.log("Tie Game")
+    }
+    else if(computer == "rock" && human =="scissor"){
+        console.log("You lose")
+        computerScore += 1
+    }
+    else if(computer == "rock" && human == "paper"){
+        console.log("You win")
+        humanScore += 1
+    }
+    else if(computer == "paper" && human =="scissor"){
+        console.log("You win")
+        humanScore += 1
+    }
+    else if(computer == "paper" && human == "rock"){
+        console.log("You lose")
+        computerScore += 1
+    }
+    else if(computer == "scissor" && human =="rock"){
+        console.log("You win")
+        humanScore += 1
+    }
+    else if(computer == "scissor" && human == "paper"){
+        console.log("You lose")
+        computerScore += 1
+    }
+    console.log("Computer: ", computer, " | ", "Human: ", human)
+    console.log("Current Score:")
+    console.log("Computer: ", computerScore, " | Human: ", humanScore )
+}
+
+function playGame(){
+    for(let i = 0; i < 5; i++){
+        console.log("Round: ", i+1)
+        playRound()
+    }
+}
+playGame()
