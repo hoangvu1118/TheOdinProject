2aed2220b25595235766eef027fb10a3d8ffc681
hoangvu1118 <mhoang.vuduc@gmail.com>
2025-07-20 23:26:19 +0700

    Computer random choice for Rock/Paper/Scissor

--- Project-Assignment/RockPaperScissor/logic.js
+++ Project-Assignment/RockPaperScissor/logic.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+let randomNumber;
+let userChoice;
+function getComputerChoice(){
+    randomNumber = Math.floor(Math.random() * 3) + 1;
+    if(randomNumber == 1){
+        return "Rock"
+    }
+    else if(randomNumber==2){
+        return "Paper"
+    }
+    else{
+        return "Scissor"
+    }
+}
+console.log(getComputerChoice())
