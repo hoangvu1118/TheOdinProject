2a2255bdc113592e84934332e8d612a95c822048
hoangvu1118 <mhoang.vuduc@gmail.com>
2025-07-21 23:37:52 +0700

    Display image choice for both user and computer

--- Project-Assignment/RockPaperScissor/logic.js
+++ Project-Assignment/RockPaperScissor/logic.js
@@ -62,26 +62,70 @@ let buttons = document.querySelector(".userChoice")
 //     userScore.value = humanScore
 // })
 
-
-
-
-buttons.addEventListener('click', (event) => {
-    let target = event.target
-    switch(target.id){
-        case "rock":
-            playRound("rock")
-            break
-        case "paper":
-            playRound("paper")
-            break
-        case "scissor":
-            playRound("scissor")
-            break
-    }
+function updateScore(){
     let compScore = document.querySelector(".element.computerScore")
     let userScore = document.querySelector(".element.userScore")
     
     compScore.textContent = `Score: ${computerScore}`
     userScore.textContent = `Score: ${humanScore}`
-    
+}
+
+function displayImage(userChoice){
+    let computer = document.querySelector(".element.Compimg")
+    let user = document.querySelector(".element.Userimg")
+
+    let imgComp = document.createElement("img");
+    let imgUser = document.createElement("img");
+
+    user.innerHTML = ""
+    if(userChoice == 1){
+        imgUser.src = "assets/rock.jpg"
+    }
+    else if(userChoice == 2){
+        imgUser.src = "assets/paper.jpg"
+    }
+    else if(userChoice == 3){
+        imgUser.src = "assets/scissor.jpg"
+    }
+    imgUser.width = 350
+    imgUser.height = 250
+    user.appendChild(imgUser)
+// Computer Image
+
+    computer.innerHTML = ""
+    if(computerChocie == "Rock"){
+        imgComp.src = "assets/rock.jpg"
+    }
+    else if(computerChocie == "Paper"){
+        imgComp.src = "assets/paper.jpg"
+    }
+    else if(computerChocie == "Scissor"){
+        imgComp.src = "assets/scissor.jpg"
+    }
+    imgComp.width = 350
+    imgComp.height = 250
+    computer.appendChild(imgComp)
+
+}
+
+buttons.addEventListener('click', (event) => {
+    let target = event.target
+    let choice = 0
+    switch(target.id){
+        case "rock":
+            playRound("rock")
+            choice = 1
+            break
+        case "paper":
+            playRound("paper")
+            choice = 2
+            break
+        case "scissor":
+            playRound("scissor")
+            choice = 3
+            break
+    }
+
+    updateScore()
+    displayImage(choice)
 })
