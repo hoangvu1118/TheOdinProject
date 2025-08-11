fb49cf414d453dfd907c9e18a6df5b49dea132a6
hoangvu1118 <mhoang.vuduc@gmail.com>
2025-08-11 23:54:11 +0700

    Creating layout for the casio(calculator)

--- Project-Assignment/The-Calculator/home.js
+++ Project-Assignment/The-Calculator/home.js
@@ -14,3 +14,31 @@ function multiply(a, b){
     return a * b
 }
 
+function operate(operator, a ,b){
+    switch(operator){
+        case "+":
+            return add(a,b)
+        case "-":
+            return subtract(a,b)
+        case "*":
+            return multiply(a,b)
+        case "/":
+            return divide(a,b)
+        default:
+            return "Not a valid operator\n"
+    }
+}
+
+let numberArea = document.querySelector(".number")
+function createNumberButtons(){
+    for(let i = 9; i > 0; i--){
+        let numBox = document.createElement("button")
+        numBox.classList.add("numerBox")
+        numBox.style.width = "100px"
+        numBox.style.height = "100px"
+        numBox.innerText = i
+        numberArea.appendChild(numBox)
+    }
+}
+
+createNumberButtons()
