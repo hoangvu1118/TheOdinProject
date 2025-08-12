2122615aee34645db80ca2e1f057a5a9794b3745
hoangvu1118 <mhoang.vuduc@gmail.com>
2025-08-12 16:41:48 +0700

    adding list to store the value of the number for computation

--- Project-Assignment/The-Calculator/home.js
+++ Project-Assignment/The-Calculator/home.js
@@ -42,27 +42,40 @@ function createNumberButtons(){
     })
 }
 createNumberButtons()
-
+let expression = []
+let currentText = ""
 let screen = document.querySelector(".screen")
 let clickNumber = document.querySelectorAll(".numberBox")
 clickNumber.forEach(num => {
     num.addEventListener("click", () => {
+        currentText += num.textContent
         screen.innerText += num.textContent
         screen.scrollLeft = screen.scrollWidth
-    })
-})
+    });
+});
 
 let clickOperator = document.querySelectorAll("#sign").forEach(signs => {
     signs.addEventListener("click", () => {
-        screen.innerText += " " + signs.textContent + " "
+        expression.push(parseInt(currentText))
+        currentText = ""
+        screen.innerText += " " + signs.textContent + "\u00A0"
         screen.scrollLeft = screen.scrollWidth
+        expression.push(signs.textContent)
     })
 })
 
 let clearButton = document.querySelector(".clearButton").addEventListener("click", () => {
     screen.innerText = ""
+    currentText = ""
+    expression = []
 })
 
 let zeroButton = document.querySelector(".Zero").addEventListener("click", () =>{
-    screen.innerText += 0
+    screen.innerText += "0"
+    currentText += "0"
+})
+
+let result = document.querySelector(".Enter").addEventListener("click", () =>{
+    expression.push(parseInt(currentText))
+    console.log(expression)
 })
