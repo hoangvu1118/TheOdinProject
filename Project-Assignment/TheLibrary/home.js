3ac76a8dffbf5553eb3b96545425c2f7cd8495b4
hoangvu1118 <mhoang.vuduc@gmail.com>
2025-08-22 16:19:55 +0700

    adding a box to submit new book

--- Project-Assignment/TheLibrary/home.js
+++ Project-Assignment/TheLibrary/home.js
@@ -25,4 +25,25 @@ addBookToLibrary("Crazy Memory", "Katze Wang", "162", false)
 addBookToLibrary("Super Duber", "Evan Tahan", "37", false)
 addBookToLibrary("KingKong2", "Monkey Aatz", "29", false)
 
+
+const formBox = document.querySelector("#form-box")
+let create_Book = document.querySelector(".function.add")
+create_Book.addEventListener("click", () => {
+    formBox.style.display = 'flex'
+})
+
+const booksForm = document.querySelector(".submit")
+booksForm.addEventListener("click", (e) => {
+    e.preventDefault()
+    const form = document.querySelector("#forms")
+    const formData = new FormData(form)
+    let title = formData.get("title")
+    let author = formData.get("author")
+    let pages = formData.get("pages")
+    let read = formData.get("read")
+    addBookToLibrary(title, author, pages, read)
+    console.log(title, author, pages, read)
+    formBox.style.display = 'none'
+})
+
 console.table(myLibrary)
