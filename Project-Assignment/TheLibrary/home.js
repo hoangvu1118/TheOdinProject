8c838140f47261dc3fe0014c85d2fe804574735c
hoangvu1118 <mhoang.vuduc@gmail.com>
2025-08-23 09:38:00 +0700

    adding buttons to interact with books

--- Project-Assignment/TheLibrary/home.js
+++ Project-Assignment/TheLibrary/home.js
@@ -32,18 +32,53 @@ create_Book.addEventListener("click", () => {
     formBox.style.display = 'flex'
 })
 
-const booksForm = document.querySelector(".submit")
-booksForm.addEventListener("click", (e) => {
+const booksForm = document.querySelector("#forms")
+booksForm.addEventListener("submit", (e) => {
+    if (!booksForm.checkValidity()) {
+        booksForm.reportValidity();
+        return;
+    }
     e.preventDefault()
-    const form = document.querySelector("#forms")
-    const formData = new FormData(form)
+    const formData = new FormData(booksForm)
     let title = formData.get("title")
     let author = formData.get("author")
     let pages = formData.get("pages")
-    let read = formData.get("read")
+    let read = formData.get("read") ? true : false
     addBookToLibrary(title, author, pages, read)
-    console.log(title, author, pages, read)
     formBox.style.display = 'none'
+    updateShelf()
+    numberOfBook += 1
+    booksForm.reset()
 })
+let numberOfBook = 0
+let shelf = document.querySelector(".shelf")
+function updateShelf(){
+    let i = numberOfBook
+    for (; i < myLibrary.length; i++){
+        let boxSquare = document.createElement("div")
+        boxSquare.classList.add("book")
+        shelf.appendChild(boxSquare)
+        boxSquare.textContent = myLibrary[i].title
+        shelf.scrollTop = shelf.scrollHeight
+        interactWithBook(boxSquare)
 
-console.table(myLibrary)
+    }
+}
+
+function interactWithBook(book){
+    let viewButton = document.createElement("button")
+    let deleteButton = document.createElement("button")
+    let toggleReadStatus = document.createElement("input")
+
+    toggleReadStatus.id = "checkbox"
+    const label = document.createElement("label");
+    label.htmlFor = "checkbox";
+
+
+    toggleReadStatus.type = "checkbox"
+    viewButton.classList.add("viewButton")
+    deleteButton.classList.add("deleteBook")
+    label.appendChild(toggleReadStatus)
+    label.append(" Read")
+    book.append(viewButton, deleteButton, label)
+}
